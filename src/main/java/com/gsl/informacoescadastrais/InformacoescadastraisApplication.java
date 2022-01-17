487327b9ebcf5e7d10d9f6a710688e5f43cf80c5
Pedro Henrique Braz <pedro.henrique@opus-software.com.br>
2022-01-17 17:37:01 -0300

    setup inicial projeto

---
+++ src/main/java/com/gsl/informacoescadastrais/InformacoescadastraisApplication.java
@@ -0,0 +1,13 @@
+package com.gsl.informacoescadastrais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformacoescadastraisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InformacoescadastraisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gsl/informacoescadastrais/InformacoescadastraisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsl.informacoescadastrais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformacoescadastraisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
